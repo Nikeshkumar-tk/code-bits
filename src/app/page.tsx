5d6fa18399a8a7b6f76e8bc43323e55ca92fa32b
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2023-12-29 21:47:41 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default async function Home() {
+  
+  return (
+    <main>
+      <h1>Hello world</h1>
+    </main>
+  );
+}
---
+++ src/components/site-header.tsx
@@ -0,0 +1,5 @@
+export function SiteHeader(){
+    return (
+        <header></header>
+    )
+}
