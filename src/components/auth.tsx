a8ec61ae4f66c6f014216f7f269266f477920d2d
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2024-01-01 21:46:27 +0530

    husky mod

--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -5,49 +5,47 @@ import { signIn } from "next-auth/react"
 
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { Button } from "./ui/button"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "./ui/dropdown-menu"
 
 export { SessionProvider } from "next-auth/react"
 
 export const SignInBtn = () => {
-    return (
-        <Button size={"sm"} onClick={() => signIn()}>
-            Sign In
-        </Button>
-    )
+  return (
+    <Button size={"sm"} onClick={() => signIn()}>
+      Sign In
+    </Button>
+  )
 }
 
 export const UserInfo = ({ userInfo }: { userInfo: User | undefined }) => {
-    const initials = userInfo?.name?.split("")[0]?.toUpperCase()
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button
-                    variant="secondary"
-                    className="relative h-8 w-8 rounded-full"
-                >
-                    <Avatar className="h-8 w-8">
-                        <AvatarImage
-                            src={userInfo?.image}
-                            alt={userInfo?.name ?? ""}
-                        />
-                        <AvatarFallback>{initials}</AvatarFallback>
-                    </Avatar>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56" align="end" forceMount>
-                <DropdownMenuLabel className="font-normal">
-                    <div className="flex flex-col space-y-1">
-                        <p className="text-sm font-medium leading-none">
-                            {userInfo?.name}
-                        </p>
-                        <p className="text-xs leading-none text-muted-foreground">
-                            {userInfo?.email}
-                        </p>
-                    </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                {/* <DropdownMenuGroup>
+  const initials = userInfo?.name?.split("")[0]?.toUpperCase()
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="secondary" className="relative h-8 w-8 rounded-full">
+          <Avatar className="h-8 w-8">
+            <AvatarImage src={userInfo?.image} alt={userInfo?.name ?? ""} />
+            <AvatarFallback>{initials}</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">{userInfo?.name}</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {userInfo?.email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {/* <DropdownMenuGroup>
                     <DropdownMenuItem asChild>
                         <Link href="/dashboard/stores">
                             <DashboardIcon
@@ -84,7 +82,7 @@ export const UserInfo = ({ userInfo }: { userInfo: User | undefined }) => {
                         <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
                     </Link>
                 </DropdownMenuItem> */}
-            </DropdownMenuContent>
-        </DropdownMenu>
-    )
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 }
