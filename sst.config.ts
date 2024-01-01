0bbadbb7b958c6ae76bc53cc42061160d4680d8c
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2024-01-01 22:23:57 +0530

    sst configure

---
+++ sst.config.ts
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+import { SSTConfig } from "sst"
+import { NextjsSite } from "sst/constructs"
+
+export default {
+  config(_input) {
+    return {
+      name: "code-bits",
+      region: "us-east-1",
+    }
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new NextjsSite(stack, "site")
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      })
+    })
+  },
+} satisfies SSTConfig
